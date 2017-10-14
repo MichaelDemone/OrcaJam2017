a7c0f46f0da00fe829fdaf03135179d301329d30
MichaelDemone <m_d_emone@hotmail.com>
2017-10-14 16:24:03 -0700

    Made bullets shoot everything and planes not interact with other things they shouldn't'

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Enemy : MonoBehaviour {
+
+	public int enemyMaxHP = 10;
+	public int enemyCurrentHP;
+	protected Rigidbody2D body;
+	
+	// Use this for initialization
+	void Start () {
+		enemyCurrentHP = enemyMaxHP;
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	public virtual void TalkShitGetHit(int damage)
+	{
+		print("OUCH, THAT HURT");
+		if (enemyCurrentHP > damage) {
+			enemyCurrentHP = enemyCurrentHP - damage;
+			print("My hp is now " + enemyCurrentHP);
+		} else
+			Die();
+	}
+
+	public virtual void Die()
+	{
+		print("ded");
+		Destroy(this.gameObject);
+	}
+}
--- Assets/Scripts/FlyboiAI.cs
+++ Assets/Scripts/FlyboiAI.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyboiAI : MonoBehaviour {
+public class FlyboiAI : Enemy {
 
 
     private Vector3 vLastPos;
     public float flyboiSpeed = 3f;
-    public int enemyTotalHP = 10;
-    public int enemyCurrentHP = 0;
-    private Rigidbody2D body;
+    
     public int amplitudeMod=15;
     public int wavelengthMod = 3;
     public int enemyDamage = 1;
@@ -18,12 +16,6 @@ public class FlyboiAI : MonoBehaviour {
     void Awake() {
         body = GetComponent<Rigidbody2D>();
     }
-
-    // Use this for initialization
-    void Start () {
-        vLastPos = transform.position;
-        enemyCurrentHP = enemyTotalHP;
-    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,19 +23,5 @@ public class FlyboiAI : MonoBehaviour {
         body.velocity = new Vector2(-flyboiSpeed, 0);
         transform.position = vLastPos + new Vector3(0.0f, (Mathf.Sin(Time.time*wavelengthMod))/amplitudeMod, 0.0f);
 
-    }
-
-    public void TalkShitGetHit(int damage) {
-        print("OUCH, THAT HURT");
-        if (enemyCurrentHP > damage) {
-            enemyCurrentHP = enemyCurrentHP - damage;
-            print("My hp is now " + enemyCurrentHP);
-        } else
-            Die();
-    }
-
-    public void Die() {
-        print("ded");
-        Destroy(this.gameObject);
     }
 }

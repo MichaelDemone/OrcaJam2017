abadea2d0f6380bf9b36cab77c8d4aa6993a7d3d
angushay <angushay@uvic.ca>
2017-10-14 17:53:49 -0700

    added pause text

---
+++ Assets/PauseTextScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseTextScript : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.GetComponent<PlayerController>().paused)
+        {
+            GetComponent<Text>().enabled = true;
+        } else
+        {
+            GetComponent<Text>().enabled = false;
+        }
+	}
+}

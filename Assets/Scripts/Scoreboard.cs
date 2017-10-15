dd443c7a9b1b5971591df3d452d3f5798903b3fa
MichaelDemone <m_d_emone@hotmail.com>
2017-10-15 16:25:50 -0700

    Fly bois make firerate go faster and number of placeable towers is based on score

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Scoreboard : MonoBehaviour {
 
-    public static int score = 0;
+	public static int score
+	{
+		get { return _score; }
+		set
+		{
+			_score = value;
+			Instance.SetNumTowers();
+		}
+	}
+	
+	private static int _score = 0;
 
+	public PlayerTowerInteractions pti;
+
+	private static Scoreboard Instance;
+	
 	// Use this for initialization
-	void Start () {
-        score = 0;
+	void Start () 
+	{
+		Instance = this;
+		_score = 0;
+		SetNumTowers();
+	}
+
+	void SetNumTowers()
+	{
+		pti.MaxNumberOfTowers = Mathf.FloorToInt((_score - 10f) / 10f) + 1;
+
+/*		if (_score > 10)
+		{
+			pti.MaxNumberOfTowers = Mathf.FloorToInt((_score - 10f) / 10f) + 1;
+		}
+		else
+		{
+			pti.MaxNumberOfTowers = 1;
+		}*/
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        print(score);
+        //print(score);
 	}
 }

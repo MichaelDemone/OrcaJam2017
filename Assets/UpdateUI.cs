d9e5fc1b67e46b56409f64afb95027400a6326d1
MichaelDemone <m_d_emone@hotmail.com>
2017-10-15 15:31:16 -0700

    Did scoreboard stuff and made some other ui pretty af

--- Assets/UpdateUI.cs
+++ Assets/UpdateUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,15 @@ using UnityEngine.UI;
 public class UpdateUI : MonoBehaviour
 {
 
+    public Sprite[] Numbers;
+
+    public Image Matey1;
+    public Image Matey2;
+    public Image TowerMax;
+    public Image TowerCurrent;
+    public Image Enemy1;
+    public Image Enemy2;
+    
     public GameObject mateys;
     public int NumEnemies = 0;
     public GameObject player;
@@ -22,8 +32,21 @@ public class UpdateUI : MonoBehaviour
         int mateysHP = mateys.GetComponent<TheMateys>().mateysCurrentHP;
         int numTowers = player.GetComponent<PlayerTowerInteractions>().NumberOfTowersPlaced;
         int maxTowers = player.GetComponent<PlayerTowerInteractions>().MaxNumberOfTowers;
-        gameObject.GetComponent<Text>().text = mateysHP + "\n" +
-            numTowers + "/" + maxTowers + "\n"
-            + NumEnemies;
+
+        SetNumbers(mateysHP, Matey1, Matey2);
+        TowerMax.sprite = Numbers[numTowers];
+        TowerCurrent.sprite = Numbers[maxTowers];
+        SetNumbers(NumEnemies, Enemy1, Enemy2);
+    }
+
+    void SetNumbers(int number, Image firstDigitSprite, Image secondDigitSprite)
+    {
+        number = Mathf.Clamp(number, 0, 99);
+        
+        int firstDigit = Mathf.FloorToInt(number / 10f);
+        int secondDigit = number % 10;
+
+        firstDigitSprite.sprite = Numbers[firstDigit];
+        secondDigitSprite.sprite = Numbers[secondDigit];
     }
 }

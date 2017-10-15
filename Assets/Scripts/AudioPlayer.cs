e76814291ab506341149998c07e8cac547021b00
MichaelDemone <m_d_emone@hotmail.com>
2017-10-15 10:40:28 -0700

    Made an audio player to play sounds

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+	private static AudioPlayer instance;
+	private static AudioSource audioSource;
+	
+	public static void PlayFile(AudioClip clip)
+	{
+		audioSource.PlayOneShot(clip);
+
+	}
+	
+	// Use this for initialization
+	void Start ()
+	{
+		instance = this;
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

4c3ff778dd74ce90f2a7d6ed3764f51eeed8b22f
Gregory Perry <gregoryperry627@gmail.com>
2017-10-15 18:56:47 -0700

    a

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -29,12 +29,20 @@ public class Scoreboard : MonoBehaviour {
 	}
 
 	void SetNumTowers()
-    {
-        if (pti.MaxNumberOfTowers < 5) 
-            {
-                pti.MaxNumberOfTowers = Mathf.FloorToInt((_score - 10f) / 30f) + 1;
-            } 
-            else{ pti.MaxNumberOfTowers = 5; }
+
+    {   if (score < 10) { pti.MaxNumberOfTowers = 0; }
+        else if (score < 30) { pti.MaxNumberOfTowers = 1; }
+        else if (score < 60) { pti.MaxNumberOfTowers = 2; }
+        else if (score < 120) { pti.MaxNumberOfTowers = 3; }
+        else if (score > 240) { pti.MaxNumberOfTowers = 4; }
+        else { pti.MaxNumberOfTowers = 5; }
+
+        //if (pti.MaxNumberOfTowers < 5) 
+         //   {
+        
+          //      pti.MaxNumberOfTowers = Mathf.FloorToInt((_score - 10f) / 30f);
+          //  } 
+           // else{ pti.MaxNumberOfTowers = 5; }
 		
 	}
 	

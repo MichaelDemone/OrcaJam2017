9938953a5b0ae28f564ecf5305d12a9ed7780153
twistedburger <twistedburger@hotmail.com>
2017-10-15 17:50:36 -0700

    Souuunnds

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -4,34 +4,58 @@ using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour {
 
-    public AudioClip announcerStart, enemy1, enemy2, enemy3, tower1, tower2, tower3;
+    public AudioClip announcerStart, enemy1, enemy2, tower1, tower2, tower3, parrot1, parrot2, boss1, boss2, boss3;
     List<AudioClip> listAll = new List<AudioClip>();
 
     List<AudioClip> listTower = new List<AudioClip>();
 
     List<AudioClip> listEnemy = new List<AudioClip>();
 
-	// Use this for initialization
-	void Start () {
+    List<AudioClip> listParrot = new List<AudioClip>();
+
+    List<AudioClip> listBoss = new List<AudioClip>();
+    List<AudioClip> listET = new List<AudioClip>();
+
+
+
+    // Use this for initialization
+    void Start () {
 
         StartCoroutine(Wait());
 
-        Sound(announcerStart);
+        AudioPlayer.PlayFile(announcerStart,2f);
 
         listAll.Add(enemy1);
         listAll.Add(enemy2);
-        listAll.Add(enemy3);
         listAll.Add(tower1);
         listAll.Add(tower2);
         listAll.Add(tower3);
+        listAll.Add(parrot1);
+        listAll.Add(parrot2);
+
+        listParrot.Add(parrot1);
+        listParrot.Add(parrot2);
+        listParrot.Add(enemy1);
+        listParrot.Add(enemy2);
+
+        listBoss.Add(boss1);
+        listBoss.Add(boss2);
+        listBoss.Add(boss3);
 
         listEnemy.Add(enemy1);
         listEnemy.Add(enemy2);
-        listEnemy.Add(enemy3);
 
         listTower.Add(tower1);
         listTower.Add(tower2);
         listTower.Add(tower3);
+
+        listET.Add(enemy1);
+        listET.Add(enemy2);
+
+        listET.Add(tower1);
+        listET.Add(tower2);
+        listET.Add(tower3);
+
     }
 	
 	// Update is called once per frame
@@ -55,18 +79,27 @@ public class SoundPlayer : MonoBehaviour {
     }
 
     void PlaySound() {
-        if(randomNumber() <= 0.5) {
-            if(GameObject.Find("Enemy(Clone)") != null && GameObject.Find("Tower(Clone)") != null) {
-                AudioClip sound = listAll[Random.Range(0, listAll.Count-1)];
-                Sound(sound);
-            } else if(GameObject.Find("Enemy(Clone)") != null) {
-                AudioClip sound = listEnemy[Random.Range(0, listEnemy.Count-1)];
-                Sound(sound);
-            } else if (GameObject.Find("Tower(Clone)") != null) {
-                AudioClip sound = listTower[Random.Range(0, listTower.Count-1)];
-                Sound(sound);
-            }
+
+        if (GameObject.Find("Boss(Clone)") != null) {
+            AudioClip sound = listBoss[Random.Range(0, listBoss.Count-1)];
+            Sound(sound);
+        } else if (GameObject.Find("Tower(Clone)") != null && GameObject.Find("FlyBois(Clone)") != null) {
+            AudioClip sound = listAll[Random.Range(0, listAll.Count-1)];
+            Sound(sound);
+        } else if (GameObject.Find("FlyBois(Clone)") != null && GameObject.Find("Tower(Clone)") == null) {
+            AudioClip sound = listParrot[Random.Range(0, listParrot.Count-1)];
+            Sound(sound);
+        } else if (GameObject.Find("Tower(Clone)") != null && GameObject.Find("Enemy(Clone)") == null) {
+            AudioClip sound = listTower[Random.Range(0, listTower.Count-1)];
+            Sound(sound);
+        } else if (GameObject.Find("Tower(Clone)") != null && GameObject.Find("Enemy(Clone)") != null) {
+            AudioClip sound = listET[Random.Range(0, listET.Count-1)];
+            Sound(sound);
+        } else if(GameObject.Find("Enemy(Clone)") != null) {
+            AudioClip sound = listEnemy[Random.Range(0, listEnemy.Count-1)];
+            Sound(sound);
         }
+        
     }
 
     void Sound(AudioClip sound) {
